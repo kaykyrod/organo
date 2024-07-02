a5f82f09e1c835f6bb04f6060a1dadc1e97b8e1f
Kayky <kayky.rodrigues021@outlook.com>
2024-07-01 22:58:36 -0300

    :rocket: deploy do projeto

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <div className='rodape-container'>
+            <div className='redes-sociais'>
+                <img src='./imagens/fb.png' alt='Logo do Facebook'></img>
+                <img src='./imagens/tw.png' alt='Logo do Twitter'></img>
+                <img src='./imagens/ig.png' alt='Logo do Instagram'></img>
+            </div>
+        <img src='./imagens/logo.png' alt='Logo do Organo' />
+        <p>Desenvolvido por Alura</p>
+        </div>
+    )
+}
+
+export default Rodape

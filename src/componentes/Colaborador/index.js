e1e585679481189706f101004a5c0a83d4aaab8c
Kayky <kayky.rodrigues021@outlook.com>
2024-07-03 22:08:47 -0300

    :sparkles: feat: novas funcionalidades

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,34 @@
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 import './Colaborador.css'
 
-const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
+const Colaborador = ({ nome, imagem, cargo, corDeFundo, aoDeletar, colaborador, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className='colaborador'>
+        <AiFillCloseCircle 
+            size={25} 
+            className='deletar' 
+            onClick={() => aoDeletar(colaborador.id)} 
+        />
         <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
             <img src={imagem} alt={nome}/>
         </div>
         <div className='rodape'>
             <h4>{nome}</h4>
             <h5>{cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                    ? <AiFillHeart {...propsFavorito} color='#ff0000' /> 
+                    : <AiOutlineHeart{...propsFavorito}  />
+                }
+            </div>
         </div>
     </div>)
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,28 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
+    const css = { backgroundColor: hexToRgba(props.cor, '0.6') }
 
     return (
         (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <input onChange={evento => props.mudarCor(evento.target.value, props.id)} value={props.cor} type='color' className='input-cor'></input>
+            <h3 style={{ borderColor: props.cor }}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+                {props.colaboradores.map( colaborador => {
+                    return (
+                        <Colaborador 
+                            corDeFundo={props.cor} 
+                            nome={colaborador.nome} 
+                            key={colaborador.nome} 
+                            imagem={colaborador.imagem} 
+                            cargo={colaborador.cargo} 
+                            colaborador={colaborador}
+                            aoDeletar={props.aoDeletar} 
+                            aoFavoritar={props.aoFavoritar}
+                    />)
+                })}
             </div>
         </section> 
         : ''
